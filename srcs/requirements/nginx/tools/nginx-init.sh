520a64599a6314de7fb872d67a930b2e5d31e71c
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2026-08-10 00:01:33 +0200

    More typos corrected

--- srcs/requirements/nginx/tools/nginx-init.sh
+++ srcs/requirements/nginx/tools/nginx-init.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p /etc/nginx/ssl
 
 if [ ! -f /etc/nginx/ssl/inception.crt ]; then
-	echo "Generating SSL sefl-signed certificate..."
+	echo "Generating SSL self-signed certificate..."
 	openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 		-keyout /etc/nginx/ssl/inception.key \
 		-out /etc/nginx/ssl/inception.crt \
